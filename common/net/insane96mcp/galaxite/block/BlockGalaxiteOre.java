e6a1ac27ed4a91bbf18bd3ea59ee8c951c2c9905
Alberto Del Villano <insane96mcp@gmail.com>
2018-03-24 10:06:43 +0100

    Ore now emits particles, closes #6. Added Russian language

--- common/net/insane96mcp/galaxite/block/BlockGalaxiteOre.java
+++ common/net/insane96mcp/galaxite/block/BlockGalaxiteOre.java
@@ -1,10 +1,16 @@
 package net.insane96mcp.galaxite.block;
 
+import java.util.Random;
+
 import net.insane96mcp.galaxite.Galaxite;
 import net.insane96mcp.galaxite.lib.Names;
 import net.minecraft.block.BlockOre;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockGalaxiteOre extends BlockOre{
 	
@@ -22,4 +28,63 @@ public class BlockGalaxiteOre extends BlockOre{
 	public String getUnlocalizedName() {
 		return "tile." + Galaxite.RESOURCE_PREFIX + Names.GALAXITE_ORE;
 	}
+	
+	@Override
+	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		this.spawnParticles(worldIn, pos);
+	}
+	
+	@Override
+    public int tickRate(World worldIn)
+    {
+        return 30;
+    }
+
+    private void spawnParticles(World worldIn, BlockPos pos)
+    {
+        Random random = worldIn.rand;
+        double d0 = 0.0625D;
+
+        for (int i = 0; i < 6; ++i)
+        {
+            double d1 = (double)((float)pos.getX() + random.nextFloat());
+            double d2 = (double)((float)pos.getY() + random.nextFloat());
+            double d3 = (double)((float)pos.getZ() + random.nextFloat());
+
+            if (i == 0 && !worldIn.getBlockState(pos.up()).isOpaqueCube())
+            {
+                d2 = (double)pos.getY() + 0.0625D + 1.0D;
+            }
+
+            if (i == 1 && !worldIn.getBlockState(pos.down()).isOpaqueCube())
+            {
+                d2 = (double)pos.getY() - 0.0625D;
+            }
+
+            if (i == 2 && !worldIn.getBlockState(pos.south()).isOpaqueCube())
+            {
+                d3 = (double)pos.getZ() + 0.0625D + 1.0D;
+            }
+
+            if (i == 3 && !worldIn.getBlockState(pos.north()).isOpaqueCube())
+            {
+                d3 = (double)pos.getZ() - 0.0625D;
+            }
+
+            if (i == 4 && !worldIn.getBlockState(pos.east()).isOpaqueCube())
+            {
+                d1 = (double)pos.getX() + 0.0625D + 1.0D;
+            }
+
+            if (i == 5 && !worldIn.getBlockState(pos.west()).isOpaqueCube())
+            {
+                d1 = (double)pos.getX() - 0.0625D;
+            }
+
+            if (d1 < (double)pos.getX() || d1 > (double)(pos.getX() + 1) || d2 < 0.0D || d2 > (double)(pos.getY() + 1) || d3 < (double)pos.getZ() || d3 > (double)(pos.getZ() + 1))
+            {
+                worldIn.spawnParticle(EnumParticleTypes.PORTAL, d1, d2, d3, 0.0D, 0.0D, 0.0D);
+            }
+        }
+    }
 }
