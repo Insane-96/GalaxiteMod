49f8c254d679e3b91169ec3afb1870dde8ef91ac
Insane-96 <insane96mcp@gmail.com>
2018-10-09 21:03:18 +0200

    Ore now drops gem. Closed #9

--- common/net/insane96mcp/galaxite/block/BlockGalaxiteOre.java
+++ common/net/insane96mcp/galaxite/block/BlockGalaxiteOre.java
@@ -3,13 +3,18 @@ package net.insane96mcp.galaxite.block;
 import java.util.Random;
 
 import net.insane96mcp.galaxite.Galaxite;
+import net.insane96mcp.galaxite.init.ModItems;
 import net.insane96mcp.galaxite.lib.Names;
 import net.minecraft.block.BlockOre;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.item.Item;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockGalaxiteOre extends BlockOre{
@@ -35,10 +40,39 @@ public class BlockGalaxiteOre extends BlockOre{
 	}
 	
 	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return ModItems.galaxiteItem;
+	}
+	
+	@Override
+	public int quantityDroppedWithBonus(int fortune, Random random) {
+		if (fortune > 0)
+        {
+            int i = random.nextInt(fortune + 2) - 1;
+
+            if (i < 0)
+            {
+                i = 0;
+            }
+
+            return this.quantityDropped(random) * (i + 1);
+        }
+        else
+        {
+            return this.quantityDropped(random);
+        }
+	}
+	
+	@Override
     public int tickRate(World worldIn)
     {
         return 30;
     }
+	
+	@Override
+	public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
+		return (!(entity instanceof EntityDragon));
+	}
 
     private void spawnParticles(World worldIn, BlockPos pos)
     {
